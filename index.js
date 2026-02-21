f1cb0560bb89e22903688395364a926076816484
Md Selimur Rahaman <md.selimur.rahaman.05@gmail.com>
2026-02-21 22:24:52 +0530

    added colour

--- index.js
+++ index.js
@@ -19,30 +19,47 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+//color integrated
+function getRandomColor() {
+  const colors = [
+    "#e74c3c", "#3498db", "#2ecc71",
+    "#9b59b6", "#f39c12", "#1abc9c",
+    "#e67e22", "#e84393", "#16a085"
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
 // Socket.io
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('join', (username) => {
     socket.username = username;
+    socket.color = getRandomColor(); // assign color
+
     io.emit('chat message', {
       user: "System",
-      text: `${username} joined the chat`
+      text: `${username} joined the chat`,
+      color: "#888"
     });
   });
 
   socket.on('chat message', (data) => {
-    io.emit('chat message', data);
+    io.emit('chat message', {
+      user: socket.username,
+      text: data.text,
+      color: socket.color
+    });
   });
 
   socket.on('disconnect', () => {
     if (socket.username) {
       io.emit('chat message', {
         user: "System",
-        text: `${socket.username} left the chat`
+        text: `${socket.username} left the chat`,
+        color: "#888"
       });
     }
-    console.log('User disconnected');
   });
 });
 
